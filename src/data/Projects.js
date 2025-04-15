c4fd0991e1ac82d14c806fb9aac8be485cf5f9ff
T H E 9 B I N <109237169+thenabinamallik@users.noreply.github.com>
2025-04-16 03:26:52 +0530

    Update Projects.js

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -9,28 +9,28 @@ export const projects = [
         img: BloodCheck,
         skills: ["Tailwind CSS", "React", "Javascript", "HTML", "CSS"],
         title: 'BloodCheck',
-        desc: 'A portfolio app detailing & describing about me. Basically a resume converted into website.',
+        desc: 'Detect bloodgroup uging fingerprint patterns',
         live: "/404",
         github: 'https://github.com/thenabinamallik/BloodCheck.git',
-    },
-    {
-        id: 1,
-        img: full_stack_notes,
-        skills: ["Java", "DSA", "Javascript", "MERN", "Kotlin"],
-        title: 'Fullstack_developer ',
-        date: 'june 2022 - Present',
-        desc: 'A repo you need for Programming & Problem Solving. This is contains everthing you need to be full stack web developer with DSA.',
-        live: 'https://github.com/Dharmendra6944',
-        github: 'https://github.com/Dharmendra6944'
-    },
-    {
-        id: 2,
-        img: spotify_clone,
-        skills: ["Tailwind CSS", "React", "React Icons", "Vite", "NPM"],
-        title: 'Spotify Clone',
-        date: 'February 2024 - February 2024',
-        desc: 'Home and Search pages of Spotify. This was an assignment given during job hunting. Remember, many functionalities will not work, as it is just a frontend Tailwind CSS implementation.',
-        live: 'https://open.spotify.com/',
-        github: 'https://github.com/Dharmendra6944'
-    }
+    // },
+    // {
+    //     id: 1,
+    //     img: full_stack_notes,
+    //     skills: ["Java", "DSA", "Javascript", "MERN", "Kotlin"],
+    //     title: 'Fullstack_developer ',
+    //     date: 'june 2022 - Present',
+    //     desc: 'A repo you need for Programming & Problem Solving. This is contains everthing you need to be full stack web developer with DSA.',
+    //     live: 'https://github.com/Dharmendra6944',
+    //     github: 'https://github.com/Dharmendra6944'
+    // },
+    // {
+    //     id: 2,
+    //     img: spotify_clone,
+    //     skills: ["Tailwind CSS", "React", "React Icons", "Vite", "NPM"],
+    //     title: 'Spotify Clone',
+    //     date: 'February 2024 - February 2024',
+    //     desc: 'Home and Search pages of Spotify. This was an assignment given during job hunting. Remember, many functionalities will not work, as it is just a frontend Tailwind CSS implementation.',
+    //     live: 'https://open.spotify.com/',
+    //     github: 'https://github.com/Dharmendra6944'
+    // }
 ]
