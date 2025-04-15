3eba1b033bc1e171018a71ee55af25985ad68013
T H E 9 B I N <109237169+thenabinamallik@users.noreply.github.com>
2025-04-16 03:21:46 +0530

    Update Bio.js

--- src/data/Bio.js
+++ src/data/Bio.js
@@ -8,7 +8,7 @@ export const Bio = {
         "Animator / Editor / Designer.",
     ],
     description: "I'm a passionate Full-Stack Developer and Creative Technologist with a flair for blending code with creativity. My journey encompasses Mobile Development, Web Development, AI & ML, and IoT & Networking. I strive to make meaningful impacts by combining my love for technology and creativity in various domains.",
-    resume: "https://docs.google.com/document/d/1RL6gCGOW5Iv4Meoz5Y4JiXeL16A8eZME/edit?usp=drive_link&ouid=108486224922230820090&rtpof=true&sd=true", // Replace with your actual resume link
+    resume: "https://drive.google.com/uc?export=download&id=1WY6XKBOGfwUOKkBFlMbPbWUY5aiML_kS", // Replace with your actual resume link
     github: "https://github.com/thenabinamallik", // Updated GitHub link
     linkedin: "https://www.linkedin.com/in/thenabinamallik", // Updated LinkedIn link
     twitter: "https://twitter.com/thenabinamallik", // Updated Twitter link
