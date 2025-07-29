597824243b295e278a9751fb08e1aca8d40f5a6e
T H E 9 B I N <109237169+thenabinamallik@users.noreply.github.com>
2025-07-29 19:42:27 +0530

    Update Skills.js

--- src/data/Skills.js
+++ src/data/Skills.js
@@ -15,14 +15,14 @@ export const skills = [
             },
             {
                 id: 1.4,
-                name: 'Java',
+                name: "Java",
                 image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
             },
             {
                 id: 1.5,
                 name: "JavaScript",
-                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
-            },
+                image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+            }
         ]
     },
     {
@@ -31,7 +31,7 @@ export const skills = [
         skill: [
             {
                 id: 2.1,
-                name: 'Firebase',
+                name: "Firebase",
                 image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
             },
             {
@@ -47,25 +47,24 @@ export const skills = [
         skill: [
             {
                 id: 3.1,
-                name: "React Js",
-                image: 'https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png',
+                name: "React JS",
+                image: "https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png",
             },
             {
                 id: 3.2,
-                name: "Next js",
-                image: 'https://seeklogo.com/images/N/next-js-icon-logo-EE302D5DBD-seeklogo.com.png',
+                name: "Next JS",
+                image: "https://seeklogo.com/images/N/next-js-icon-logo-EE302D5DBD-seeklogo.com.png",
             },
             {
                 id: 3.3,
-                name: "Express Js",
-                image: 'https://th.bing.com/th/id/OIP.1ji9NLQl3sOXktSoEYnt3wHaHa?pid=ImgDet&rs=1',
-            }
-            ,
+                name: "Express JS",
+                image: "https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png",
+            },
             {
                 id: 3.4,
                 name: "React Native",
-                image: 'https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png'
-
+                image: "https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png"
+            }
         ]
     },
     {
@@ -75,17 +74,17 @@ export const skills = [
             {
                 id: 4.2,
                 name: "Figma",
-                image: "https://s3-figma-hubfile-images-production.figma.com/hub/file/carousel/img/b8d00b2e389e3a38f115ea00f7dc3dff3ef4f337/dbc32aa496f21ffa8508a4f6432d1e6c6f76e3b1"
+                image: "https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg",
             },
             {
                 id: 4.3,
                 name: "Tailwind CSS",
-                image: 'https://logowik.com/content/uploads/images/tailwind-css3232.logowik.com.webp',
+                image: "https://logowik.com/content/uploads/images/tailwind-css3232.logowik.com.webp",
             },
             {
                 id: 4.6,
                 name: "CSS",
-                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png',
+                image: "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
             },
             {
                 id: 4.7,
@@ -95,8 +94,8 @@ export const skills = [
             {
                 id: 4.8,
                 name: "XML",
-                image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
-            },
+                image: "https://upload.wikimedia.org/wikipedia/commons/8/8d/Xml-icon.svg",
+            }
         ]
     },
     {
@@ -113,12 +112,11 @@ export const skills = [
                 name: "GitHub",
                 image: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
             },
-            
             {
                 id: 5.5,
                 name: "Vercel",
-                image: "https://yt3.ggpht.com/a/AATXAJyIDBA4e3bojKFwANXtkLdQJ2E9tmAhRDJtcw=s900-c-k-c0xffffffff-no-rj-mo",
-            },
+                image: "https://assets.vercel.com/image/upload/front/favicon/vercel/favicon.ico",
+            }
         ]
     },
     {
@@ -133,25 +131,23 @@ export const skills = [
             {
                 id: 6.2,
                 name: "VS Code",
-                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+                image: "https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg",
             },
             {
                 id: 6.3,
                 name: "Postman",
-                image:
-                    "https://www.svgrepo.com/show/354202/postman-icon.svg",
+                image: "https://www.svgrepo.com/show/354202/postman-icon.svg",
             },
             {
                 id: 6.4,
                 name: "MS Office",
-                image: "https://i.pinimg.com/736x/93/6d/6a/936d6adc03927c1e2b386060e222c918.jpg"
+                image: "https://i.pinimg.com/736x/93/6d/6a/936d6adc03927c1e2b386060e222c918.jpg",
             },
             {
                 id: 6.5,
                 name: "Arduino IDE",
-                image : "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Arduino_IDE_logo.svg/768px-Arduino_IDE_logo.svg.png?20230510165732"
+                image: "https://upload.wikimedia.org/wikipedia/commons/7/73/Arduino_IDE_logo.svg",
             }
-
         ]
     }
 ];
