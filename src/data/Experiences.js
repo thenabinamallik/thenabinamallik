33383afef78e740e80629ed53f0ba2d06e3371f6
T H E 9 B I N <109237169+thenabinamallik@users.noreply.github.com>
2025-07-29 19:06:08 +0530

    Update Experiences.js

--- src/data/Experiences.js
+++ src/data/Experiences.js
@@ -1,44 +1,7 @@
-
-import SharpCareer from '../assets/sharpcareers.png'
-import EiSystems from '../assets/eisystems.png'
 import Zidiodevelopment from '../assets/zidiodevelopment.png'
 
 export const experiences = [
-    {
-        id: 0,
-        img: SharpCareer,
-        role: "Full Stack Developer Intern",
-        company: "SharpCareer Technologies",
-        date: "June 2024 - July 2024",
-        desc: "Created a hospitality website for a hotel. The website is a full stack web app. I am working on the frontend of the project. I am using ReactJS, NodeJs, ExpressJS, MongoDB, Tailwind CSS for the project.",
-        skills: [
-            "ReactJS",
-            "NodeJs",
-            "HTML",
-            "CSS",
-            "JavaScript",
-            "MongoDB",
-            "ExpressJS",
-            "Tailwind CSS"
-        ],
-    },
-    {
-        id: 1,
-        img: EiSystems,
-        role: "Python Developer Intern",
-        company: "EiSystems",
-        date: "May 2024 - June 2024",
-        desc: "helping community , learning & growing together. I am working on a project which is a web app for a company. It is a full stack web app. I am working on the backend of the project.",
-        skills: [
-            "Python",
-            "Django",
-            "Flask",
-            "JavaScript",
-            "HTML",
-            "CSS",
-            'Tailwind CSS',
-        ],
-    },
+   
     {
         id: 2,
         img: Zidiodevelopment,
